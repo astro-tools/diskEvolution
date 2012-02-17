c22338fb0c78306c2e7683d3f48ed9be885e4a67
John Shumway <john.shumwayjr@gmail.com>
2012-02-17 11:31:41 -0700

    Some cleanup.

--- src/edu/asu/sese/diskEvolution/DiskSimulation.java
+++ src/edu/asu/sese/diskEvolution/DiskSimulation.java
@@ -5,6 +5,7 @@ public class DiskSimulation {
 
     private static RadialGrid radialGrid;
     private static DensityGrid densityGrid;
+    private static ViscosityGrid viscosityGrid;
 
     public static void main(String[] args) {
         double rmin = 0.1 * PhysicalConstants.auInCm;
@@ -12,8 +13,12 @@ public class DiskSimulation {
         double deltar0 = 0.1 * PhysicalConstants.auInCm;
         int intervalCount = 50;
         radialGrid = new RadialGrid(rmin , rmax , deltar0 , intervalCount);
+        
         densityGrid = new DensityGrid(radialGrid);
         densityGrid.initializeWithPowerLaw(1e3, PhysicalConstants.auInCm, -1.5);
+        
+        viscosityGrid = new ViscosityGrid(radialGrid);
+        viscosityGrid.initializeWithPowerLaw(1e12, PhysicalConstants.auInCm, 1.0)
         
         System.out.print("Running DiskSimulation");
         
