572517b9617abb86f1d03cfcb10827156eb01d64
user <prajkta87@gmail.com>
2012-09-17 10:19:26 -0700

    Added Drop down Menu bar

--- src/edu/asu/sese/diskEvolution/view/GraphicalApplication.java
+++ src/edu/asu/sese/diskEvolution/view/GraphicalApplication.java
@@ -2,10 +2,16 @@ package edu.asu.sese.diskEvolution.view;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import edu.asu.sese.diskEvolution.controller.DiskSimulation;
@@ -41,13 +47,20 @@ public class GraphicalApplication extends JFrame {
         view = new DiskView(simulation);
         add(view, BorderLayout.CENTER);
     }
-
     public void setupMenuBar() {
         JMenuBar menuBar = new JMenuBar();
         System.out.println(menuBar);
         JMenu toolsMenu = new JMenu("Tools");
         menuBar.add(toolsMenu);
         setJMenuBar(menuBar);
+        
+        JMenuItem aAction = new JMenuItem("a");
+        JMenuItem bAction = new JMenuItem ("b");
+        JMenuItem cAction = new JMenuItem("c");
+        
+        toolsMenu.add(aAction);
+        toolsMenu.add(bAction);
+        toolsMenu.add(cAction);        
     }
     
-}
+	}                                   
