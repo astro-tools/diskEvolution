1467df657225f994821ee34a9237b43d7868c83a
prajkta <prajkta@10.143.7.3>
2012-10-22 11:56:06 -0700

    Replaced PhysicalConstants after problem with last merge.

---
+++ src/edu/asu/sese/diskEvolution/util/PhysicalConstants.java
@@ -0,0 +1,6 @@
+package edu.asu.sese.diskEvolution.util;
+
+public class PhysicalConstants {
+    public static final double earthRadiusInCm = 6.371e8;
+	public static final double auInCm = 1.496e13;
+}
