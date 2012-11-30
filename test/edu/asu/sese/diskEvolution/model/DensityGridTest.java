14f1465d06e6558c470492ccd672d77dee50cf7f
prajkta <prajkta@192.168.0.11>
2012-11-30 00:06:23 -0700

    Added CalculateTotalMass test to the density grid

--- test/edu/asu/sese/diskEvolution/model/DensityGridTest.java
+++ test/edu/asu/sese/diskEvolution/model/DensityGridTest.java
@@ -10,17 +10,24 @@ import edu.asu.sese.diskEvolution.util.PhysicalConstants;
 import edu.asu.sese.diskEvolution.util.RadialGrid;
 
 public class DensityGridTest {
-    DensityGrid grid;
-    RadialGrid radialGrid;
-    int zoneCount = 100;
-    
+	 double rmin;
+	 double rmax;
+	 double deltar0;
+	 double radius0;
+	 double density0;
+	 double exponent;
+	 
+	 DensityGrid grid;
+	 RadialGrid radialGrid;
+	 int zoneCount = 100;
+	     
     @Before
     public void setup() {
-        double rmin = 0.1 * PhysicalConstants.earthRadiusInCm;
-        double rmax = 40.0 * PhysicalConstants.earthRadiusInCm;
-        double deltar0 = 0.01 * PhysicalConstants.earthRadiusInCm;
-        radialGrid = new RadialGrid(rmin, rmax, deltar0, zoneCount);
-        grid = new DensityGrid(radialGrid);
+       rmin = 0.1 * PhysicalConstants.earthRadiusInCm;
+       rmax = 40.0 * PhysicalConstants.earthRadiusInCm;
+       deltar0 = 0.01 * PhysicalConstants.earthRadiusInCm;
+       radialGrid = new RadialGrid(rmin, rmax, deltar0, zoneCount);
+       grid = new DensityGrid(radialGrid);
     }
     
     @Test
@@ -32,8 +39,7 @@ public class DensityGridTest {
     public void testGetValue() {
         assertEquals(0.0, grid.getValue(1), 1e-14);
     }
-
-    
+   
     @Test
     public void testInitialization() {
         grid.initializeWithPowerLaw(1.0e3, PhysicalConstants.earthRadiusInCm, -1.5);
@@ -41,4 +47,16 @@ public class DensityGridTest {
         double expected = 1.0e3 * Math.pow(rcheck, -1.5);
         assertEquals(expected, grid.getValue(0), 1e-10);
     }
-}
+    
+//	@Test
+//  public void testTotalMassIsZeroWhenDensityIsZero(){
+//		assertEquals(0.0, grid.getTotalMass(), 1e-12);
+//	}
+	
+    @Test
+    public void testTotalMass() {
+        double mass = 1.52e21;
+        assertEquals(mass, grid.getTotalMass(), 1e19);
+    }
+
+ }
