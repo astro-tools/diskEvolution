8030bf6790f5f88733345e57b7978b04f0238b43
John Shumway <john.shumwayjr@gmail.com>
2012-12-03 11:46:01 -0700

    Added a new test for initializing disk with rin and rout, still need to implement.

--- src/edu/asu/sese/diskEvolution/model/DensityGrid.java
+++ src/edu/asu/sese/diskEvolution/model/DensityGrid.java
@@ -18,10 +18,17 @@ public class DensityGrid extends MidpointGrid {
     
     public double getTotalMass() {
         double mass = 0.0;
-        for (int i = 1; i < zoneCount; ++i) {
+        for (int i = 0; i < zoneCount; ++i) {
             mass += getValue(i) * getArea(i);
         }
         return mass;
+    }
+
+    public void initializeWithPowerLaw(double density0, double rmin, double d,
+            double rin, double rout) {
+        // Note: this ignores rin and rout!
+        super.initializeWithPowerLaw(density0, rmin, d);
+        // Now need to set empty bins (inside rin or outside rout) to zero.
     }
 
 }
--- test/edu/asu/sese/diskEvolution/model/DensityGridTest.java
+++ test/edu/asu/sese/diskEvolution/model/DensityGridTest.java
@@ -55,11 +55,22 @@ public class DensityGridTest {
 	}
 	
     @Test
-    public void testTotalMass() {
+    public void testTotalMassForUniformDensity() {
         double density0 = 1.0e3;
         grid.initializeWithPowerLaw(density0, rmin, 0.0);
         double expect = Math.PI * (rmax * rmax - rmin * rmin) * density0;
-        assertEquals(expect, grid.getTotalMass(), 1e19);
+        assertEquals(expect, grid.getTotalMass(), 1e10);
+    }
+    
+    @Ignore
+    @Test 
+    public void testTotalMassWithRinAndRout() {
+        double density0 = 1.0e3;
+        double rin = 1.2 * PhysicalConstants.earthRadiusInCm;
+        double rout = 6.0 * PhysicalConstants.earthRadiusInCm;
+        grid.initializeWithPowerLaw(density0, rmin, 0.0, rin, rout);
+        double expect = Math.PI * (rout * rout - rin * rin) * density0;
+        assertEquals(expect, grid.getTotalMass(), 1e10);
     }
 
  }
