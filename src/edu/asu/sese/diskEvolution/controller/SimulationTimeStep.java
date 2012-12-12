b6d70e7ec74b38cebbea6acede19c70f06833031
prajkta <prajkta@192.168.0.11>
2012-12-11 22:29:19 -0700

    Initialized timestep, calculated Mdot for each boundary

--- src/edu/asu/sese/diskEvolution/controller/SimulationTimeStep.java
+++ src/edu/asu/sese/diskEvolution/controller/SimulationTimeStep.java
@@ -2,25 +2,26 @@ package edu.asu.sese.diskEvolution.controller;
 
 import edu.asu.sese.diskEvolution.model.DensityGrid;
 import edu.asu.sese.diskEvolution.model.MassFlowGrid;
+import edu.asu.sese.diskEvolution.model.ViscosityGrid;
 import edu.asu.sese.diskEvolution.util.RadialGrid;
 
 public class SimulationTimeStep {
 	  private MassFlowGrid massFlowGrid;
 	    private RadialGrid radialGrid;
 	    private DensityGrid densityGrid;
+	    private ViscosityGrid viscosityGrid;
 
 	
-public int timeMax = 20;
+public int timeMax = 20; 
 public double timestep;
+public double mdot;
 
 	public void intializeTimeStep() {
 		for (int i = 0; i < timeMax; ++i) {
 			timestep = 0.0;
-//			timestep += timestep + timestep*getMdot(i);
-			timestep = timestep + 0.02739726027; //10*days/year
-			
-			
-			
+			mdot = 3*Math.PI * densityGrid.getValue(i)* viscosityGrid.getValue(i);
+			timestep +=  timestep* mdot;
+		
 		}
 	}
 	
