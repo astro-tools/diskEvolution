7222becabded0c555fcca8048228056207be3faf
prajkta <prajkta@10.143.44.231>
2012-12-12 10:55:42 -0700

    instead of calculating the accretion rate, imported it from massFlowCalculator

--- src/edu/asu/sese/diskEvolution/controller/SimulationTimeStep.java
+++ src/edu/asu/sese/diskEvolution/controller/SimulationTimeStep.java
@@ -1,27 +1,23 @@
 package edu.asu.sese.diskEvolution.controller;
 
-import edu.asu.sese.diskEvolution.model.DensityGrid;
+
 import edu.asu.sese.diskEvolution.model.MassFlowGrid;
-import edu.asu.sese.diskEvolution.model.ViscosityGrid;
 import edu.asu.sese.diskEvolution.util.RadialGrid;
 
 public class SimulationTimeStep {
 	  private MassFlowGrid massFlowGrid;
 	    private RadialGrid radialGrid;
-	    private DensityGrid densityGrid;
-	    private ViscosityGrid viscosityGrid;
 
-	
-public int timeMax = 20; 
-public double timestep;
-public double mdot;
 
-	public void intializeTimeStep() {
-		for (int i = 0; i < timeMax; ++i) {
+	    public double timestep;
+	    public double mdot;
+	    public int intervalCount = radialGrid.getIntervalCount();
+
+	public void TimeStep() {
+		for (int i = 0; i < intervalCount; ++i) {
 			timestep = 0.0;
-			mdot = 3*Math.PI * densityGrid.getValue(i)* viscosityGrid.getValue(i);
+			mdot = massFlowGrid.getValue(i);
 			timestep +=  timestep* mdot;
-		
 		}
 	}
 	
