63026905a74dcc51fc102b17eafaaa41f003bfb1
prajkta <prajkta@10.143.44.231>
2012-12-12 15:27:58 -0700

    gets timStep value from Time Step

--- src/edu/asu/sese/diskEvolution/model/MassMover.java
+++ src/edu/asu/sese/diskEvolution/model/MassMover.java
@@ -6,6 +6,7 @@ import edu.asu.sese.diskEvolution.util.RadialGrid;
 public class MassMover {
 	  private MassFlowGrid massFlowGrid;
 	  private DensityGrid densityGrid;
+	  private Double timeStep;
 
 
 	public MassMover(DensityGrid density, MassFlowGrid massFlow,
@@ -14,19 +15,25 @@ public class MassMover {
 	public double mdot1, mdot2;
 	public double moveMass() {
 		int count = massFlowGrid.getCount();
-		double timeStep = 10.0;
+		Double step = getTimeStep();
 		double value1, value2, difference, density = 0.0;
 		for (int i=1; i<count; ++i) {
 
 		 mdot1 = massFlowGrid.getValue(i);
 		 mdot2 = massFlowGrid.getValue(i+1);
-		 value1 = mdot1*timeStep;
-		 value2 = mdot2*timeStep;
+		 value1 = mdot1*step;
+		 value2 = mdot2*step;
 		 difference = value2 - value1;
 		 density = densityGrid.getValue(i);
 		 density += difference;
 		}
 		return density;
+	}
+	public double getTimeStep() {
+		return timeStep;
+	}
+	public void setTimeStep(Double timeStep) {
+		this.timeStep = timeStep;
 	}
 	
 
