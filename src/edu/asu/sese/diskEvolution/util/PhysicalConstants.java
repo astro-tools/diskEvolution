c615a1d9c00557f3a485f45c0b301ca07bc1a7cc
John Shumway <john.shumwayjr@gmail.com>
2012-12-22 01:31:37 -0700

    Fixed value of the moon's mass in PhysicalConstants.

--- src/edu/asu/sese/diskEvolution/util/PhysicalConstants.java
+++ src/edu/asu/sese/diskEvolution/util/PhysicalConstants.java
@@ -3,7 +3,7 @@ package edu.asu.sese.diskEvolution.util;
 public class PhysicalConstants {
     public static final double earthRadiusInCm = 6.371e8;
 	public static final double auInCm = 1.496e13;
-    public static double lunarMass = 7.34767309e22;
+    public static double lunarMass = 7.34767309e25;
     public static double year = 31557600.0;
     public static double day = 24.0 * 60.0 * 60.0;
 }
