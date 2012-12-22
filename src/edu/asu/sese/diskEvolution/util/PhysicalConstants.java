12026928ff3c00bc752258ba18f3de6171c1d40b
John Shumway <john.shumwayjr@gmail.com>
2012-12-22 01:42:30 -0700

    Added hour and minute to PhysicalConstants.

--- src/edu/asu/sese/diskEvolution/util/PhysicalConstants.java
+++ src/edu/asu/sese/diskEvolution/util/PhysicalConstants.java
@@ -6,4 +6,6 @@ public class PhysicalConstants {
     public static double lunarMass = 7.34767309e25;
     public static double year = 31557600.0;
     public static double day = 24.0 * 60.0 * 60.0;
+    public static double hour = 60.0 * 60.0;
+    public static double minute = 60.0;
 }
