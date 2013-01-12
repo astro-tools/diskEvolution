323b0251b385c2f0aa99172bee68199c2d0d024c
John Shumway <john.shumwayjr@gmail.com>
2013-01-11 17:01:17 -0700

    Added Snapshot and SnapshotCollection classes.

---
+++ src/edu/asu/sese/diskEvolution/model/Snapshot.java
@@ -0,0 +1,12 @@
+package edu.asu.sese.diskEvolution.model;
+
+import edu.asu.sese.diskEvolution.controller.DiskSimulation;
+
+public class Snapshot {
+
+    DensityGrid density;
+    
+    Snapshot(DiskSimulation simulation) {
+        density = new DensityGrid(simulation.getDensityGrid());
+    }
+}
