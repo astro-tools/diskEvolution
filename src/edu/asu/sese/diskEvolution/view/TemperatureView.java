9cae02b9ef6cced5935908732f6d6d953778d70a
prajkta <prajkta@10.143.237.131>
2013-11-19 11:08:12 -0700

    working on Temperature View: (NullPointerException error)

--- src/edu/asu/sese/diskEvolution/view/TemperatureView.java
+++ src/edu/asu/sese/diskEvolution/view/TemperatureView.java
@@ -1,14 +1,4 @@
-//package edu.asu.sese.diskEvolution.view;
-//
-//import javax.swing.JComponent;
-//import javax.swing.JPanel;
-//
-//import edu.asu.sese.diskEvolution.plot.ArrayGrid;
-//import edu.asu.sese.diskEvolution.plot.PlotView;
-//import edu.asu.sese.diskEvolution.util.PhysicalConstants;
-//import edu.asu.sese.diskEvolution.util.Unit;
-//
-//
+
 package edu.asu.sese.diskEvolution.view;
 
 import org.jfree.chart.ChartPanel;
@@ -21,72 +11,20 @@ import edu.asu.sese.diskEvolution.util.MidpointAdaptor;
 import edu.asu.sese.diskEvolution.util.PhysicalConstants;
 import edu.asu.sese.diskEvolution.util.Unit;
 public class TemperatureView extends JPanel{
+
 	private static final long serialVersionUID = 1L;
-//	
-//    private PlotView plotView;
-//    private ArrayGrid timePlotGrid;
-//	private ArrayGrid massPlotGrid;
-//
-//
 	
-//	
-//    void createGraph() {
-//        createTimeGrid();
-//        Unit year = new Unit("yr", "yr", PhysicalConstants.year);
-//        createMassGrid();
-//        Unit lunarMass = new Unit("M☽", "M<sub>☽</sub>",
-//                PhysicalConstants.lunarMass);
-//        plotView = new PlotView(timePlotGrid, massPlotGrid, "t", "M_tot", year, lunarMass);
-//        double tmax = 30.0 * PhysicalConstants.year;
-//        double mmax = 1.8 * PhysicalConstants.lunarMass;
-//        plotView.setAxisLimits(0.0, tmax, 0.0, mmax);
-//    }
-//
-//    private void createMassGrid() {
-//        double [] mass = new double [31];
-//        for (int i = 0; i < 31; i++){
-//        	mass[i] = 0.8 * PhysicalConstants.lunarMass;
-//        
-//        }
-//        massPlotGrid = new ArrayGrid(mass);
-//    }
-//    
-//
-//    private void createTimeGrid() {
-//        double [] time = new double [31];
-//        for (int i = 0; i < 31; ++i) {
-//            time[i] = i * PhysicalConstants.year;
-//        }
-//        timePlotGrid = new ArrayGrid(time);
-//    }
-//
-//    public JComponent getComponent() {
-//        return plotView.getChartPanel();
-//    }
-//
-//}
-
-
-
-
-  
-    
-    private PlotView plot;
+	private PlotView plot;
     private UnitInterface domainUnit;
     private UnitInterface rangeUnit;
     private TemperatureGrid temperatureGrid;
 
-   // public TemperatureView(TemperatureGrid temperatureGrid) {  
-      //  this.temperatureGrid = temperatureGrid;
-     //   createTemperatureGraphs();
-   // }
-    
+
     public TemperatureView(){
 		createGraph();
-	//all the methods like make the temperature chart, labels and Field etc
 		}
 
-    private void createGraph() {
+    private void createGraph(){ 
         MidpointAdaptor radialGrid = new MidpointAdaptor(temperatureGrid.getRadialGrid());
         String domainLabel = "r";
         String rangeLabel = "T<sub>eff</sub>";
@@ -94,8 +32,6 @@ public class TemperatureView extends JPanel{
                 PhysicalConstants.earthRadiusInCm);
         rangeUnit = new Unit("K", "K", 1.0);
         plot = new PlotView(radialGrid, temperatureGrid, domainLabel, rangeLabel, domainUnit, rangeUnit);
-        plot.makeRangeAxisLogarithmic();
-
     }
 
     public  ChartPanel getComponent() {
